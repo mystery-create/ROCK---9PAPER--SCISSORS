36c7da6c66125c4e3d4eec75fc360aef8314cfdb
mystery-create <princerajkumar678@gmail.com>
2026-01-29 20:11:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,68 @@
+let userScore=0;
+let computerScore=0;
+const msg=document.querySelector("#msg");
+const user=document.querySelector("#user-score");
+const com=document.querySelector("#comp-score");
+const choices=document.querySelectorAll(".choice");
+const genCompchoice=() =>{
+    const options=["rock","paper","scissor"]
+    //rock,paper,scissors
+    let idx=Math.floor(Math.random()*3);
+    return options[idx];
+}
+ const drawGame =() => {
+    console.log("draw game");
+    msg.innerText ="!!! DRAW !!!";
+     msg.style.backgroundColor="#081b31";
+    
+};
+const showWinner=(userWin) =>{
+    if(userWin){
+        console.log("you win!");
+    msg.innerText ="you win!!";
+    userScore++;
+   msg.style.backgroundColor="green";
+   user.innerText=userScore;
+   com.innerText=computerScore;
+                    }
+        else {
+            console.log("you lose.");
+              msg.innerText ="you loss.";
+              computerScore++;
+              msg.style.backgroundColor="red";
+              user.innerText=userScore;
+   com.innerText=computerScore;
+        }
+    
+};
+const playgame=(userchoice) => {
+    console.log("user choices",userchoice);
+     //generate computer choices -> modular 
+     const compchoice=genCompchoice();
+     console.log("computer choices",compchoice);
+     if(userchoice===compchoice){
+        drawGame();
+     }else{
+        let userWin=true;
+        if(userchoice==="rock"){
+            //paper,scissor
+            userWin=compchoice==="paper"?false:true;}
+            else if(userchoice==="paper"){
+                //rock,scissor
+                compchoice==="rock" ?true:false;
+                } else{
+                    //rock,paper
+                 userWin=compchoice==="rock" ?false:true;
+                }
+          showWinner(userWin);
+        }
+     
+};
+choices.forEach((choice) => {
+   // console.log(choice);
+    choice.addEventListener("click",() =>{
+        const userchoice=choice.getAttribute("id");
+       
+       playgame(userchoice);
+    });
+});
